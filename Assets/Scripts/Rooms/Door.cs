38301f91c9abcd323942eb92d8babda3fe38a190
Eliott <eliott.chauchard@orange.fr>
2023-03-07 17:57:14 +0100

    Locks and rooms

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -7,13 +7,36 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public GameObject DoorGO;
-    public bool opened;
-    
+    public bool opened, locked;
+    [HideInInspector] public List<Room> linkedRooms = new List<Room>();
+
+    private void Start()
+    {
+        CheckIfLocked();
+    }
+
     public void ToggleDoor()
     {
+        if (locked) return;
+        
         opened = !opened;
         
         if(DoorGO) DoorGO.SetActive(!opened);
         else transform.GetChild(0).gameObject.SetActive(!opened);
+    }
+
+    public void CheckIfLocked()
+    {
+        foreach (var room in linkedRooms)
+        {
+            if (room.IsLocked)
+            {
+                locked = true;
+                return;
+            }
+        }
+
+        Debug.Log("no");
+        locked = false;
     }
 }
--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool locked;
+
+    public bool IsLocked => locked;
+
+    [SerializeField] private Door[] doors;
+
+    private void Awake()
     {
-        
+        foreach (var door in doors)
+        {
+            door.linkedRooms.Add(this);
+            door.locked = locked;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("UnlockDoor")]
+    public void UnlockDoor()
     {
-        
+        locked = false;
+        foreach (var door in doors)
+        {
+            door.locked = false;
+        }
     }
 }
