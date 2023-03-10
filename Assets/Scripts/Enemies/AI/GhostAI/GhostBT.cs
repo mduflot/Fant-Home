75d4d7601c9da0dd50e7f63edd4d27c562857b1c
Issa Gandega <issagndega94@gmail.com>
2023-03-10 15:55:21 +0100

    new weapons + better waves

---
+++ Assets/Scripts/Enemies/AI/GhostAI/GhostBT.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using BehaviorTree;
+using UnityEngine;
+using Tree = BehaviorTree.Tree;
+
+namespace AI.GhostAI
+{
+    public class GhostBT : Tree
+    {
+        [SerializeField] float _speed = 2f;
+        [SerializeField] private float _fovRange = 6f;
+        [SerializeField] private float _attackRange = 4f;
+        [SerializeField] private float _attackTime = 2f;
+
+        public static float Speed;
+        public static float FOVRange;
+        public static float AttackRange;
+
+        protected override Node SetupTree()
+        {
+            Speed = _speed;
+            FOVRange = _fovRange;
+            AttackRange = _attackRange;
+            
+            Node root = new Selector(new List<Node>
+            {
+                new Sequence(new List<Node>
+                {
+                    new CheckPlayerInAttackRange(transform),
+                    new TaskAttack(transform, _attackTime),
+                }),
+                new Sequence(new List<Node>
+                {
+                    new CheckPlayer(transform),
+                    new TaskGoToTarget(transform),
+                })
+            });
+
+            return root;
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptables/WeaponsSO.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public enum BulletKeys
+{
+    Chain,
+    BigBertha
+}
+
+public enum BulletTypes
+{
+    Classic,
+    Explosive
+}
+
+[CreateAssetMenu(fileName = "WeaponStats", menuName = "Scriptables/WeaponStats", order = 2)]
+public class WeaponsSO: ScriptableObject
+{
+    public float bulletSpeed;
+    public float reloadTime;
+    public BulletKeys key;
+    public BulletTypes type;
+}
