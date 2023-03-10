593bb6a37dcb2fbe493c73ab3f745e91daa283bd
Maxime Duflot <maximedft@outlook.com>
2023-03-10 21:39:22 +0100

    feat: refactor ghost, refactor SO;

--- Assets/Scripts/Enemies/Ghost.cs
+++ Assets/Scripts/Enemies/Ghost.cs
@@ -1,23 +1,46 @@
 using System.Collections;
+using Scriptables;
 using UnityEngine;
 
 public class Ghost : MonoBehaviour, IEnemy
 {
-    [SerializeField] private string _name = "Trash";
-    [SerializeField] private float _health = 3;
-    [SerializeField] private float _veil = 1;
-    [SerializeField] private int _durationStun;
-    [SerializeField] private int _damage = 1;
-    [SerializeField] private int _speed = 5;
+    [SerializeField] private GhostStatsSO _ghostSO;
+    [SerializeField] private string _name = "Ghost";
 
     [HideInInspector] public bool IsStun;
 
+    private float _health = 3;
+    private float _shield = 1;
+    private int _durationStun;
+    private int _damage = 1;
+    private int _speed = 5;
+
+    private bool _isVulnerable;
+
+    private void Start()
+    {
+        gameObject.name = _name;
+        _health = _ghostSO.MaxHealth;
+        _shield = _ghostSO.MaxVeil;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet") && _isVulnerable)
+        {
+            Pooler.instance.Depop(collision.gameObject.GetComponent<Bullet>().key, collision.gameObject);
+            // TODO: TakeDamage from Weapon.Damage
+            // TakeDamage();
+        }
+    }
+
     public void TakeVeil(float damageVeil)
     {
-        _veil -= damageVeil;
-        if (_veil <= 0)
+        _shield -= damageVeil;
+        if (_shield <= 0)
         {
             IsStun = true;
+            _isVulnerable = true;
             StartCoroutine(StunDuration());
         }
     }
---
+++ Assets/Scripts/Scriptables/PoltergeistStatsSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = "new PoltergeistStatsSO", menuName = "Scriptables/GhostsSO/PoltergeistStatsSO")]
+    public class PoltergeistStatsSO : GhostStatsSO
+    {
+        public float DetectionRange;
+        public float UnitMakingCD;
+        public int NbrUnitsToSpawn;
+    }
+}
