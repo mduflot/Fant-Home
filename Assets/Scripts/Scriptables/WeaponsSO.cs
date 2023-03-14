adda41068165e75e36af2e428cad8dd83a20aa8c
Issa GANDEGA <issagndega94@gmail.com>
2023-03-14 11:34:02 +0100

    start new weapons

--- Assets/Scripts/Scriptables/WeaponsSO.cs
+++ Assets/Scripts/Scriptables/WeaponsSO.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Unity.Entities;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public enum BulletKeys
@@ -11,12 +15,14 @@ public enum BulletKeys
 public enum BulletTypes
 {
     Classic,
-    Explosive
+    Explosive,
+    Triple
 }
 
 [CreateAssetMenu(fileName = "WeaponStats", menuName = "Scriptables/WeaponStats", order = 2)]
 public class WeaponsSO: EquipmentSO
 {
+    public int damage;
     public float bulletSpeed;
     public float reloadTime;
     [Range(0, 45f)]
@@ -24,4 +30,36 @@ public class WeaponsSO: EquipmentSO
     public BulletKeys key;
     public BulletTypes type;
     public FlashLightSO flashLight;
+    
+    
+}
+
+[CustomEditor(typeof(WeaponsSO)), CanEditMultipleObjects]
+public class WeaponEditor : Editor
+{
+    private SerializedProperty iDamage;
+    private SerializedProperty fSpeed;
+    private SerializedProperty fReload;
+    private SerializedProperty fSpread;
+    private SerializedProperty bkKey;
+    private SerializedProperty btType;
+    private SerializedProperty fsoFlashlight;
+
+    private void OnEnable()
+    {
+        iDamage = serializedObject.FindProperty("damage");
+        iDamage = serializedObject.FindProperty("bulletSpeed");
+        iDamage = serializedObject.FindProperty("reloadTime");
+        iDamage = serializedObject.FindProperty("bulletSpread");
+        iDamage = serializedObject.FindProperty("key");
+        iDamage = serializedObject.FindProperty("type");
+        iDamage = serializedObject.FindProperty("flashlight");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(fSpread);
+        base.OnInspectorGUI();
+    }
 }
