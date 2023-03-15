cf4c38da50088899536561fb5f5bc9e2ed7db144
Maxime Duflot <maximedft@outlook.com>
2023-03-15 09:24:31 +0100

    feat: add interactable;

---
+++ Assets/Scripts/Enemies/AI/PoltergeistAI/CheckInteractable.cs
@@ -0,0 +1,69 @@
+﻿using BehaviorTree;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace AI.PoltergeistAI
+{
+    public class CheckInteractable : Node
+    {
+        private Transform _transform;
+        private float _interactableCounter;
+        private float _interactableCD;
+        private float _radius;
+        
+        public CheckInteractable(Transform transform, float interactableCD, float radius)
+        {
+            _transform = transform;
+            _interactableCD = interactableCD;
+            _radius = radius;
+        }
+        
+        public override NodeState Evaluate()
+        {
+            object isInteract = GetData("isInteract");
+
+            if (isInteract != null)
+            {
+                _state = NodeState.SUCCESS;
+                return _state;
+            }
+            
+            _interactableCounter += Time.deltaTime;
+            if (_interactableCounter >= _interactableCD)
+            {
+                int currentIndex = 0;
+                float previousDistance = 0;
+                
+                Collider[] hitColliders = Physics.OverlapSphere(_transform.position, _radius);
+
+                if (hitColliders.Length < 1)
+                {
+                    _state = NodeState.FAILURE;
+                    return _state;
+                }
+                
+                for (var index = 0; index < hitColliders.Length; index++)
+                {
+                    var hitCollider = hitColliders[index];
+                    if (hitCollider.transform.CompareTag("Interactable") || hitCollider.transform.CompareTag("Wall"))
+                    {
+                        float currentDistance =
+                            math.sqrt(math.lengthsq(_transform.position - hitCollider.transform.position));
+                        if (currentDistance < previousDistance || previousDistance == 0)
+                        {
+                            previousDistance = currentDistance;
+                            currentIndex = index;
+                        }
+                    }
+                }
+                
+                Parent.Parent.SetData("object", hitColliders[currentIndex].transform);
+                _state = NodeState.SUCCESS;
+                return _state;
+            }
+
+            _state = NodeState.FAILURE;
+            return _state;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/AI/PoltergeistAI/TaskInteractable.cs
@@ -0,0 +1,74 @@
+﻿using BehaviorTree;
+using UnityEngine;
+
+namespace AI.PoltergeistAI
+{
+    public class TaskInteractable : Node
+    {
+        private Transform _transform;
+        private float _attackRange;
+        private float _speed;
+        
+        public TaskInteractable(Transform transform, float attackRange, float speed)
+        {
+            _transform = transform;
+            _attackRange = attackRange;
+            _speed = speed;
+        }
+        
+        public override NodeState Evaluate()
+        {
+            Transform targetObject = (Transform)GetData("object");
+
+            if (targetObject.GetComponent<Interuptor>())
+            {
+                if (Vector3.Distance(_transform.position, targetObject.position) > _attackRange)
+                {
+                    _transform.position = Vector3.MoveTowards(
+                        _transform.position, targetObject.position, _speed * Time.deltaTime);
+                    _transform.LookAt(targetObject.position);
+                }
+                else
+                {
+                    ClearData("object");
+                    _state = NodeState.FAILURE;
+                    return _state;
+                }
+            }
+            else if (targetObject.GetComponent<Door>())
+            {
+                if (Vector3.Distance(_transform.position, targetObject.position) > _attackRange)
+                {
+                    _transform.position = Vector3.MoveTowards(
+                        _transform.position, targetObject.position, _speed * Time.deltaTime);
+                    _transform.LookAt(targetObject.position);
+                }
+                else
+                {
+                    ClearData("object");
+                    _state = NodeState.FAILURE;
+                    return _state;
+                }
+            }
+            else
+            {
+                if (targetObject.GetComponent<Animation>())
+                {
+                    targetObject.GetComponent<Animation>().Play();
+                }
+                else
+                {
+                    // TODO: Instantiate ParticleSystem on object
+                    // Pooler.instance.Pop();
+                }
+                
+                ClearData("object");
+                _state = NodeState.SUCCESS;
+                return _state;
+            }
+            
+            _state = NodeState.RUNNING;
+            return _state;
+        }
+    }
+}
