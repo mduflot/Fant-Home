54db28599696bbd1302ac3e291981277c3c2549c
Eliott <eliott.chauchard@gmail.com>
2023-03-16 02:01:02 +0100

    UI Options + pause finish

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,23 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject PauseGO;
+    [SerializeField] private GameObject pauseGO;
     [SerializeField] private UIManager uiMana;
     [SerializeField] private GameObject firstFocus;
-    private bool active;
+    [SerializeField] private GameObject[] menuButtons;
+    [SerializeField] private GameObject optionsGO, optionsFirstFocus;
+    private bool active, optionsActive;
     
     public void TogglePause()
     {
+        Reset();
         active = !active;
-        PauseGO.SetActive(active);
+        pauseGO.SetActive(active);
         if (active)
         {
             uiMana.FocusOn(firstFocus);
         }
-
         Time.timeScale = active?0:1;
+    }
+
+    public void ToggleOptions()
+    {
+        optionsActive = !optionsActive;
+        foreach (var button in menuButtons)
+        {
+            button.SetActive(!optionsActive);
+        }
+        optionsGO.SetActive(optionsActive);
+        uiMana.FocusOn(optionsActive? optionsFirstFocus : firstFocus);
+    }
+
+    private void Reset()
+    {
+        foreach (var buttons in menuButtons)
+        {
+            buttons.SetActive(true);
+        }
+        if(optionsActive) ToggleOptions();
     }
 }
